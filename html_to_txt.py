200b109fa7132e676273d8eb026e48e502b609c0
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-28 15:34:52 +0200

    langsam wirds besser

---
+++ html_to_txt.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+ocn = ['old', 'change', 'new']
+
+url = 'file:///C:/Users/user/Bachelor/out/AktG/Nr0_2021-08-12/'
+url += ocn[1] + '.html'
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html,features="lxml")
+text = soup.get_text()
+
+#open text file
+text_file = open("C:/Users/user/Bachelor/out/AktG/Nr0_2021-08-12/change.txt", "w")
+ 
+#write string to file
+text_file.write(text)
+ 
+#close file
+text_file.close()
+print('Done')
