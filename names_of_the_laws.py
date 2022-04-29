dd7dceb33eb8276b7aa81df94d66d01e09f42bc8
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-29 17:16:11 +0200

    Ist jetzt Funktion

--- names_of_the_laws.py
+++ names_of_the_laws.py
@@ -5,18 +5,18 @@ from natsort import natsorted
 fname = 'out/'
 laws = np.loadtxt(fname + "done_with.txt", dtype=str,encoding='utf-8').tolist()
 
-for law in laws:
-    
-    names_of_change = listdir(fname + law + '/')
-    names_of_change = natsorted(names_of_change)
-    if '.DS_Store' in names_of_change:
-        names_of_change.remove('.DS_Store')
+def name_of_laws():
+    for law in laws:
         
-    text_file = open(fname + law + '/changes.txt', "w")
-    for ele in names_of_change:
-        text_file.write(ele + '\n')
-    text_file.close()
-    
+        names_of_change = listdir(fname + law + '/')
+        names_of_change = natsorted(names_of_change)
+        if '.DS_Store' in names_of_change:
+            names_of_change.remove('.DS_Store')
+        
+        text_file = open(fname + law + '/changes.txt', "w")
+        for ele in names_of_change:
+            text_file.write(ele + '\n')
+        text_file.close()
 
-print('Done')
+
 
