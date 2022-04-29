ca68e2a69f41c4e7941a96fef779b48760bb6206
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-29 16:01:06 +0200

    Namen von den Gesetzen und den Änderungen fertig

--- names_of_the_laws.py
+++ names_of_the_laws.py
@@ -5,17 +5,18 @@ from natsort import natsorted
 fname = 'out/'
 laws = np.loadtxt(fname + "done_with.txt", dtype=str,encoding='utf-8').tolist()
 
-
 for law in laws:
-    print(law)
+    
     names_of_change = listdir(fname + law + '/')
-    names_of_change.remove('.DS_Store')
     names_of_change = natsorted(names_of_change)
-    text_file = open(fname + law + 'name.txt', "w")
-    for ele in 
-    n = text_file.write('Welcome to pythonexamples.org')
+    if '.DS_Store' in names_of_change:
+        names_of_change.remove('.DS_Store')
+        
+    text_file = open(fname + law + '/changes.txt', "w")
+    for ele in names_of_change:
+        text_file.write(ele + '\n')
     text_file.close()
-    break
+    
 
 print('Done')
 
