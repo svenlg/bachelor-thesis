10b10a2dac9c4cfbd4c4689a3e2b00b5f83ea547
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-06-08 21:28:21 +0200

    main

--- main_MLM.py
+++ main_MLM.py
@@ -49,13 +49,13 @@ val_loader = DataLoader(val_dataset, batch_size=8, shuffle=True)
 optim = torch.optim.Adam(model.parameters(), lr=5e-5)
 
 # num_train_epochs
-num_train_epochs = 2
+num_train_epochs = 250
 
-train_loop(model, train_loader, val_loader, optim, device, show=1, save=300, epochs=num_train_epochs)
+train_loop(model, train_loader, val_loader, optim, device, show=1, save=10, epochs=num_train_epochs)
 
 #loss = evaluate(model, test_loader, device)
 #print(loss)
 
 print(f'Done')
 duration = time.time() - took
-print(f'Took: {duration} sec')
+print(f'Took: {duration/60} min')
