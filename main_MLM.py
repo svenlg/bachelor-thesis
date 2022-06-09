f1583a101cd4e71583791fbe25bf1aad749a143d
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-06-09 13:49:32 +0200

    test

--- main_MLM.py
+++ main_MLM.py
@@ -13,7 +13,7 @@ def main():
 
     # Getting the trainings device
     use_cuda = torch.cuda.is_available()
-    device = torch.device('cuda:0' if use_cuda else 'cpu')
+    device = torch.device('cuda:1' if use_cuda else 'cpu')
     torch.backends.cudnn.benchmark = True
     print(f'The model is trained on a {device}.\n')
 
