91866f01ca035a9ba07ca62b31ca34bfac6d0509
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-06-09 15:17:43 +0200

    training on 4 GPUs

--- train_eval_loop.py
+++ train_eval_loop.py
@@ -64,10 +64,9 @@ def train_loop(model, train_loader, val_loader, optim, device, show=1, save=40, 
             
             # trainings step
             outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-            print(outputs)
+           
             # loss
             loss = outputs[0].mean()
-            print(loss)
             
             # backward pass and gradient step
             loss.backward()
