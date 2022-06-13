35ff5114a278bf24ee80fb4660cfdb5adcb09549
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-06-13 11:15:19 +0200

    syntax

--- main_test.py
+++ main_test.py
@@ -16,9 +16,7 @@ def main(ba_size, tr_epochs, save):
     use_cuda = torch.cuda.is_available()
     device = torch.device('cuda:0' if use_cuda else 'cpu')
     torch.backends.cudnn.benchmark = True
-    print(f'test')
-    print(f'moin {use_cuda}')
-    print(f"The model is trained on {torch.cuda.device_count()} {device}.")
+    print(f"The model is trained on {torch.cuda.device_count()} {device}.\n")
 
     # Pretrained model
     checkpoint = 'dbmdz/bert-base-german-cased'
