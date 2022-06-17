fb6a09f95b59c7f633a1df9946ae6b4f0d410b35
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-06-17 11:11:27 +0200

    tokens erneuert

--- html_to_tensor.py
+++ html_to_tensor.py
@@ -3,7 +3,7 @@ import torch
 from transformers import AutoTokenizer
 import numpy as np
 from bs4 import BeautifulSoup
-import urllib.request
+import os
 
 ###
 # All files are saved as np arrays 
@@ -15,54 +15,86 @@ import urllib.request
 ###
 
 checkpoint = "dbmdz/bert-base-german-cased"
-
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
-def html_to_str_to_tensor(url, save):
-    html = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(html, features="html.parser")
-    text = soup.get_text()
+def new_save(url,save) -> None: #(url, save):
     
-    tokens_np = tokenizer.encode_plus(
-            text, add_special_tokens=False,
-            return_tensors='np')
+    # url example: '../Data_Laws/AktG/Nr0_2021-08-12/'
+    # save example: 'AktG/Nr0_2021-08-12/'
+    old = url + 'old.html'
+    old_oG = url + 'old_oG.html'
+    new = url + 'new.html'
+    new_oG = url + 'new_oG.html'
+    change = url + 'change.html'
     
-    tensor = tokens_np.input_ids[0]
-    save_path = '../Data_Laws/' + save
+    if os.path.exists(old_oG):
+        old = old_oG
+        
+    if os.path.exists(new_oG):
+        new = new_oG
     
-    #np.save(save_path, tensor)
+    with open(old,encoding='utf-8') as fp:
+        oldsoup = BeautifulSoup(fp, 'html.parser')
+    
+    with open(new, encoding='utf-8') as fp:
+        newsoup = BeautifulSoup(fp, 'html.parser')
+        
+    with open(change, encoding='utf-8') as fp:
+        chasoup = BeautifulSoup(fp, 'html.parser')
+    
+    old_text = oldsoup.get_text()
+    new_text = newsoup.get_text()
+    cha_text = chasoup.get_text()
+    
+    old_tokens_np = tokenizer.encode_plus(old_text, add_special_tokens=False,return_tensors='np')
+    new_tokens_np = tokenizer.encode_plus(new_text, add_special_tokens=False,return_tensors='np')
+    cha_tokens_np = tokenizer.encode_plus(cha_text, add_special_tokens=False,return_tensors='np')
+    
+    old_tensor = old_tokens_np.input_ids[0]
+    new_tensor = new_tokens_np.input_ids[0]
+    cha_tensor = cha_tokens_np.input_ids[0]
+    
+    old_save_path = '../Data_Token/' + save + 'old.npy'
+    new_save_path = '../Data_Token/' + save + 'new.npy'
+    cha_save_path = '../Data_Token/' + save + 'change.npy'
+    
+    np.save(old_save_path, old_tensor)
+    np.save(new_save_path, new_tensor)
+    np.save(cha_save_path, cha_tensor)
     
     return
 
-def get_old_change_new(law):
+def old_cha_new(fname, law) -> None:
+    
+    path = fname + law
+    changes = np.loadtxt(path + 'changes.txt', dtype=str, encoding='utf-8')
+    
+    if changes.shape == ():
+        change = str(changes)
+        get = path + change + '/'
+        save = law + change + '/'
+        new_save(get, save)
+        return
 
-    law = str(law)
-    fname = '../Data_Laws/' + law + '/'
-    path = 'file:///C:/Users/user/Bachelor/Data_Laws/' + law + '/'
-    changes = np.loadtxt(fname + 'changes.txt', dtype=str, encoding='utf-8')
-    k = changes[7]
-
-    for change in changes:
-        change = str(change)
-        s_path = law + '/'+ change
-        html_to_str_to_tensor(path + change + '/old.html', 
-                              s_path +'/old')
-        html_to_str_to_tensor(path + change + '/change.html', 
-                              s_path +'/change')
-        html_to_str_to_tensor(path + change + '/new.html',
-                              s_path + '/new')
+    for i in range(changes.shape[0]):
+        change = str(changes[i])
+        get = path + change + '/'
+        save = law + change + '/'
+        new_save(get, save)
         
     return
 
-def get_data():
+def token_oG() -> None:
 
     fname = '../Data_Laws/'
     laws = np.loadtxt(fname + 'done_with.txt', dtype=str, encoding='utf-8')
 
     for i in range(len(laws)):
-        print(laws[i], i)
-        get_old_change_new(laws[i])
+        law = str(laws[i])
+        print(i, law)
+        law = law + '/'
+        old_cha_new(fname, law)
     
     return
 
-# get_data()
+#token_oG()
