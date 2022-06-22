4b67ec81e5426d8131b80bd6e52a0c37e2c034c2
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-06-22 16:57:46 +0200

    schneller??

--- eval.py
+++ eval.py
@@ -15,23 +15,24 @@ def evaluate(model, val_loader, device):
         num_eval_samples = 0
         for batch in val_loader:
             
-
             # get batches 
-            input_ids = batch['input_ids'].to(device)
+            input_ids_cpu = batch['input_ids']
+            input_ids_gpu = batch['input_ids'].to(device)
             attention_mask = batch['attention_mask'].to(device)
-            labels = batch['labels'].to(device)
+            labels_cpu = batch['labels']
+            labels_gpu = batch['labels'].to(device)
             
             # outputs -> loss, logits 
             # lofits.shape = batch_size, 512, vocsize
-            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            outputs = model(input_ids_gpu, attention_mask=attention_mask, labels=labels_gpu)
 
             # Get the loss and the prediction
             loss = outputs[0].mean()
             pred = np.argmax(outputs[1].to('cpu').numpy(),axis=-1)
             
             # Get the prdictet and true tokens words for the Masked (104) Tokens
-            y_true = labels[np.where(input_ids.to('cpu') == 104)].to('cpu').numpy()
-            y_pred = pred[np.where(input_ids.to('cpu') == 104)]
+            y_true = labels_cpu[np.where(input_ids_cpu = batch['input_ids'] == 104)]
+            y_pred = pred[np.where(input_ids_cpu == 104)]
             
             #Calutate the Accuracy and the f1-scores
             acc = accuracy_score(np.ndarray.flatten(y_true), 
@@ -39,7 +40,7 @@ def evaluate(model, val_loader, device):
             f1 = f1_score(np.ndarray.flatten(y_true), 
                           np.ndarray.flatten(y_pred), average='weighted')
             
-            num_samples_batch = input_ids.shape[0]
+            num_samples_batch = input_ids_gpu.shape[0]
             num_eval_samples += num_samples_batch
             val_loss_cum += loss * num_samples_batch
             val_acc += acc * num_samples_batch
