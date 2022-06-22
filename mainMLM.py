efec36c866fbe92014ba8437d9dd9370f8b1dcae
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-06-22 17:34:25 +0200

    ws

--- mainMLM.py
+++ mainMLM.py
@@ -12,8 +12,9 @@ from pympler import asizeof
 import warnings
 warnings.filterwarnings('ignore')
 
+
 def main():
-    name = input(f'Name of the Try: \n')
+    name = input(f'Name of the Try: ')
     took = time.time()
 
     # Getting the trainings device
@@ -30,7 +31,7 @@ def main():
     laws = get_laws()
     print(f'The laws are {asizeof.asizeof(laws)/1_000_000} MB.')
     train_laws, val_laws = train_test_split(laws, test_size=.2)
-    
+
     train_dataset = LawDatasetForMLM(train_laws, 3000)
     val_dataset = LawDatasetForMLM(val_laws, 1080)
 
@@ -53,15 +54,15 @@ def main():
         num_train_epochs = 300
     else:
         num_train_epochs = 1
-        
+
     train_loop(model, train_loader, val_loader, optim, device,
                show=1, save=25, epochs=num_train_epochs, name=name)
 
     print(f'Done')
     duration = time.time() - took
     print(f'Took: {duration/60} min')
-    
-    
+
+
 if __name__ == '__main__':
     main()
-    
+
