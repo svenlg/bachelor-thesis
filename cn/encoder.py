d9afee82cab48d1ab4dcedb94e078c093110f480
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-07-04 09:30:37 +0200

    encoder und embedder

--- cn/encoder.py
+++ cn/encoder.py
@@ -1,3 +1,4 @@
+#Imports
 import torch
 import torch.nn as nn
 
@@ -7,13 +8,19 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
         self.BERT = model_loaded.model.bert
 
-    def forward(self, input_ids, attention_mask, token_type_ids = None):
+    def forward(self, input_ids, attention_mask):
         # iput batch musst at least have: 'input_ids' && 'attention_mask'
-        if token_type_ids == None:
-            outputs = self.BERT(input_ids, attention_mask=attention_mask)
-            outputs = outputs['last_hidden_state']
-        else:
-            outputs = self.BERT(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-            outputs = outputs['last_hidden_state']
-            
+        outputs = self.BERT(input_ids, attention_mask=attention_mask)
+        outputs = outputs['last_hidden_state']
         return outputs
+
+
+class Embedder(nn.Module):
+    def __init__(self,model_loaded):
+        super(Embedder, self).__init__()
+        self.embeddings = model_loaded.model.bert.embeddings
+
+    def forward(self, input_ids):
+        outputs = self.embeddings(input_ids)
+        return outputs
+
