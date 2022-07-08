bd5355e8b937db4070c86f118aefe111bc4901e3
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-07-08 22:33:26 +0200

    train

--- Code/train_COPY_ddp.py
+++ Code/train_COPY_ddp.py
@@ -124,21 +124,23 @@ def train(rank, args):
         #      f'accuracy_score:  {acc:.4f}\n'
         #      f'f1_score:        {f1:.4f}\n', flush=True)
         
-        # if cur_low_val_eval > val_loss and epoch > 3:
-        #     cur_low_val_eval = val_loss
-        #     best_round = epoch
-        #     save_path = f'/scratch/sgutjahr/log/{name}_COPY_best.pt'
-        #     torch.save({'checkpoint': checkpoint,
-        #                 'epoch': epoch,
-        #                 'model_state_dict': encoder_decoder.module.state_dict(),
-        #                 'loss': cur_low_val_eval,}, save_path)
+        if cur_low_val_eval > val_loss and epoch > 3:
+            cur_low_val_eval = val_loss
+            best_round = epoch
+            save_path = f'/scratch/sgutjahr/log/{args.model_name}_COPY_best.pt'
+            torch.save({'epoch': epoch,
+                        'model_state_dict': encoder_decoder.module.state_dict(),
+                        'loss': cur_low_val_eval}, save_path)
+            
+        if epoch % 2 == 0:
+            np.save(f'/scratch/sgutjahr/log/{args.model_name}_COPY_train_epoch_{epoch}.npy', np.array(loss_train))
+            np.save(f'/scratch/sgutjahr/log/{args.model_name}_COPY_val_epoch_{epoch}.npy', np.array(loss_val))
 
-        break
+
+    print(f'Lowest validation loss: {cur_low_val_eval:.4f} in Round {best_round}')
+    np.save(f'/scratch/sgutjahr/log/{args.model_name}_COPY_train.npy', np.array(loss_train))
+    np.save(f'/scratch/sgutjahr/log/{args.model_name}_COPY_val.npy', np.array(loss_val))
     
-    #print(f'Lowest validation loss: {cur_low_val_eval:.4f} in Round {best_round}')
-    #np.save(f'/scratch/sgutjahr/log/{name}_COPY_train.npy', np.array(loss_train))
-    #np.save(f'/scratch/sgutjahr/log/{name}_COPY_val.npy', np.array(loss_val))
-        
     return
 
 
