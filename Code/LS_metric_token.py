9813efd5c7e5f8c3ad72b78a563325e1c1066f24
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-07-14 14:55:02 +0200

    hä

--- Code/LS_metric_token.py
+++ Code/LS_metric_token.py
@@ -92,8 +92,8 @@ for i in range(4):
             stats.append([LD, LD_r])
             to = np.vstack((tar,out))
             tokens.append(to)
-            if j+1 % 25 == 0:
-                print(f'Round: {j+1}')
+            # if j+1 % 25 == 0:
+            print(f'Round: {j+1}')
         
         save_stats = f'/scratch/sgutjahr/log/LSD/{i}_stats_{se}.npy'
         save_token = f'/scratch/sgutjahr/log/LSD/{i}_token_{se}.npy'
